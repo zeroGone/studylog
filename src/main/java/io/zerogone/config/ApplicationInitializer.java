7612c2b8ef9ac8f059ee21dcdb4ff0135e2766c1
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-09 18:41:31 +0900

    initial commit

---
+++ src/main/java/io/zerogone/config/ApplicationInitializer.java
@@ -0,0 +1,31 @@
+package io.zerogone.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    private static final String ROOT_PACKAGE = "io.zerogone";
+
+    private static final String DISPATCHER_NAME = "dispatcher";
+    private static final int DISPATCHER_LOAD_NUMBER = 1;
+    private static final String DISPATCHER_MAPPING_URL = "/";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.scan(ROOT_PACKAGE);
+
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic appServlet = servletContext.addServlet(DISPATCHER_NAME, new DispatcherServlet(new GenericWebApplicationContext()));
+        appServlet.setLoadOnStartup(DISPATCHER_LOAD_NUMBER);
+        appServlet.addMapping(DISPATCHER_MAPPING_URL);
+    }
+}
---
+++ src/main/java/io/zerogone/config/WebConfiguration.java
@@ -0,0 +1,32 @@
+package io.zerogone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "io.zerogone")
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+}
