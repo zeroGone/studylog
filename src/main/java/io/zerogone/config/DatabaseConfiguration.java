a77507a64da8e101ee239db53585367ee048a942
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-10 20:45:15 +0900

    feat: 데이터베이스 설정 추가
    1. DatabaseConfiguration 클래스 추가 : DatabaseSource Bean 등록, EntityManagerFactory Bean 등록
    2. DatabaseConfiguration Test 추가

---
+++ src/main/java/io/zerogone/config/DatabaseConfiguration.java
@@ -0,0 +1,55 @@
+package io.zerogone.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DatabaseConfiguration {
+    private static final String MODEL_PACKAGE_LOCATION = "io.zerogone.model";
+
+    private final Environment environment;
+
+    public DatabaseConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("spring.datasource.driver-class-name")));
+        dataSource.setUrl(Objects.requireNonNull(environment.getProperty("spring.datasource.url")));
+        dataSource.setUsername(Objects.requireNonNull(environment.getProperty("spring.datasource.username")));
+        dataSource.setPassword(Objects.requireNonNull(environment.getProperty("spring.datasource.password")));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory
+                = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setPackagesToScan(MODEL_PACKAGE_LOCATION);
+
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactory.setJpaProperties(getJpaProperties());
+        return entityManagerFactory;
+    }
+
+    private Properties getJpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", Objects.requireNonNull(environment.getProperty("hibernate.dialect")));
+        properties.setProperty("hibernate.show_sql", Objects.requireNonNull(environment.getProperty("hibernate.show_sql")));
+        return properties;
+    }
+}
