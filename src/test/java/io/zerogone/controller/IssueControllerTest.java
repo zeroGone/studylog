8baa5d8ec9f3a47c47b40aff6e4fb299d259f3c1
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-11 17:58:31 +0900

    feat: IssueController 생성

---
+++ src/test/java/io/zerogone/controller/IssueControllerTest.java
@@ -0,0 +1,40 @@
+package io.zerogone.controller;
+
+import io.zerogone.config.WebConfiguration;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = WebConfiguration.class, loader = AnnotationConfigWebContextLoader.class)
+@WebAppConfiguration
+public class IssueControllerTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testIssuesPage() throws Exception {
+        Assert.assertNotNull(webApplicationContext.getBean(IssueController.class));
+        mockMvc.perform(get("/issues")).andExpect(status().isOk()).andExpect(view().name("issue_list"));
+    }
+}
