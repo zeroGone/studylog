bde42391cb369796b02cd23ccb7950fba52a3210
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-11 16:06:54 +0900

    feat: 이메일로 유저 조회 추가
    1.UserRepository: 유저가 존재하지 않을 경우 NoResultException 발생
    2.User Entity 임시로 설정

---
+++ src/main/java/io/zerogone/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package io.zerogone.repository;
+
+import io.zerogone.model.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+@Repository
+public class UserRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public User findUserByEmail(String email) throws NoResultException {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(root.get("email"), email));
+
+        return entityManager.createQuery(criteriaQuery).getSingleResult();
+    }
+}
