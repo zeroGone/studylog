4a9c4bee0fd38c1e5509aac832cdcf8507f8c2fe
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-19 18:45:46 +0900

    fix: semicolon missing

--- src/main/java/io/zerogone/blogmember/exception/MemberRoleConvertException.java
+++ src/main/java/io/zerogone/blogmember/exception/MemberRoleConvertException.java
@@ -2,6 +2,6 @@ package io.zerogone.blogmember.exception;
 
 public class MemberRoleConvertException extends IllegalArgumentException {
     public MemberRoleConvertException(String message) {
-        super(message)
+        super(message);
     }
 }
