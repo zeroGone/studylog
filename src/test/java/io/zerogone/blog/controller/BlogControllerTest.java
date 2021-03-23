84d33ff781459e7f907d3eaa5ce0646109f9a7dc
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-23 16:26:24 +0900

    feat: 블로그 조회 추가
    블로그 이름으로 조회, header에 블로그 이름 맵핑 및 유저 이름 닉네임 맵핑

---
+++ src/test/java/io/zerogone/blog/controller/BlogControllerTest.java
@@ -0,0 +1,51 @@
+package io.zerogone.blog.controller;
+
+import io.zerogone.config.WebConfiguration;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = WebConfiguration.class, loader = AnnotationConfigWebContextLoader.class)
+@WebAppConfiguration
+public class BlogControllerTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void handleBlogMainPage() throws Exception {
+        mockMvc.perform(get("/studylog"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("main"))
+                .andDo(print());
+
+        mockMvc.perform(get("/test"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("main"))
+                .andDo(print());
+
+        mockMvc.perform(get("/test123213212312312321123"))
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+}
