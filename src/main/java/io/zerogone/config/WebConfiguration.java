391f3e23e336eb8163f4a3ecde1b9ecc9712b61e
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-24 19:37:34 +0900

    feat: CommonsMultipartResolver Bean 등록
    MultipartFile을 사용하기 위한 resolver

--- src/main/java/io/zerogone/config/WebConfiguration.java
+++ src/main/java/io/zerogone/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package io.zerogone.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,6 +15,8 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @ComponentScan(basePackages = "io.zerogone")
 public class WebConfiguration extends WebMvcConfigurerAdapter {
+    private static final int TEN_MEGA_BYTE = 10 * 1024 * 1024;
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -28,5 +31,13 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
         registry.addResourceHandler("/img/**").addResourceLocations("/img/");
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(TEN_MEGA_BYTE);
+        multipartResolver.setMaxUploadSizePerFile(TEN_MEGA_BYTE);
+        return multipartResolver;
     }
 }
