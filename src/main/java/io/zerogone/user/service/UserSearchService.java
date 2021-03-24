72ade4bedd2741afc2551a9837210697cb182f71
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-24 12:29:07 +0900

    feat: UserVo.java 추가
    User 엔티티의 읽기 전용 클래스, UserSearchService 에서 해당 클래스로 맵핑, UserSearchService Dao 필드 이름 변경, 해당 api 테스트 수정

--- src/main/java/io/zerogone/user/service/UserSearchService.java
+++ src/main/java/io/zerogone/user/service/UserSearchService.java
@@ -1,20 +1,20 @@
 package io.zerogone.user.service;
 
-import io.zerogone.user.model.User;
-import io.zerogone.user.repository.UserSearchDao;
+import io.zerogone.user.model.UserVo;
+import io.zerogone.user.repository.UserDao;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.NoResultException;
 
 @Service
 public class UserSearchService {
-    private final UserSearchDao userSearchDao;
+    private final UserDao userDao;
 
-    public UserSearchService(UserSearchDao userSearchDao) {
-        this.userSearchDao = userSearchDao;
+    public UserSearchService(UserDao userDao) {
+        this.userDao = userDao;
     }
 
-    public User getUserHasEmail(String email) throws NoResultException {
-        return userSearchDao.findUserByEmail(email);
+    public UserVo getUserHasEmail(String email) throws NoResultException {
+        return new UserVo(userDao.findUserByEmail(email));
     }
 }
