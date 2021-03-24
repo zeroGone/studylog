7e0cb24e8fd495a6e56cdd57bf6390ba3d3540c3
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-24 21:02:46 +0900

    fix: revert commit 및 feat/naver-login 브랜치로 이동
    주의 : classes 폴더 gitignore 추가 필요함

--- classes/artifacts/studylog_war_exploded/js/post_list.js
+++
@@ -1,8 +0,0 @@
-const card = document.querySelectorAll('.card');
-
-card.forEach((element) => {
-    const isHaveThumbnail = element.querySelector('.card-image-thumbnail');
-    if (!isHaveThumbnail) {
-        element.classList.add('not-image');
-    }
-});
