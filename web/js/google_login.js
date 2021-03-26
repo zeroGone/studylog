b5da54caddf6f298cb76bd99bc9107390f8e68f6
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-26 20:39:33 +0900

    style: google_login.js 로직 수정

--- web/js/google_login.js
+++ web/js/google_login.js
@@ -1,6 +1,5 @@
 const GOOGLE_LOGIN_INFO = {
     API_URL: "login",
-    INSTANCE_NAME: "auth2",
     INIT_OPTIONS: {
         client_id: '900965677916-dipqmfduh44cl0q623ccopn0r5ivv7s4.apps.googleusercontent.com',
         cookiepolicy: 'single_host_origin',
@@ -9,43 +8,42 @@ const GOOGLE_LOGIN_INFO = {
 }
 
 function loadGoogleApi() {
-    gapi.load(GOOGLE_LOGIN_INFO.INSTANCE_NAME, () => {
+    gapi.load("auth2", () => {
         auth2 = gapi.auth2.init(GOOGLE_LOGIN_INFO.INIT_OPTIONS);
-        document.querySelector(".google-login-button").addEventListener("click", loginByGoogleApi);
-    });
-};
 
-function loginByGoogleApi() {
-    auth2.signIn().then(() => {
-        login(auth2.currentUser.get().getBasicProfile());
-    }).catch((error) => {
-        console.error('Google Sign Up or Login Error: ', error)
+        document.querySelector(".google-login-button").addEventListener("click", () => {
+            auth2.signIn().then(login(getGoogleProfile())).catch(handleGoogleApiFail);
+        });
     });
 }
 
+function getGoogleProfile() {
+    const profile = auth2.currentUser.get().getBasicProfile();
+    return {
+        "name": profile.getName(),
+        "email": profile.getEmail(),
+        "imgUrl": profile.getImageUrl()
+    }
+}
+
+function handleGoogleApiFail(error) {
+    console.error('Google Sign Up or Login Error: ', error)
+}
+
 function login(userInfo) {
-    fetch(GOOGLE_LOGIN_INFO.API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-            },
-            body: JSON.stringify({
-                "email": userInfo.getEmail(),
-                "name": userInfo.getName(),
-                "imgUrl": userInfo.getImageUrl()
-            })
+    fetch("login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(userInfo)
+    }).then(response => {
+        if (response.status === 200) {
+            window.location.pathname = "mypage";
+        } else if (response.status === 400) {
+            window.location.pathname = "signup";
         }
-    )
-        .then(response => {
-            if (response.status === 200) {
-                window.location = "/mypage";
-            } else if (response.status === 400) {
-                alert(response.json())
-            }
-        })
-        .catch(error =>
-            alert(error)
-        );
+    }).catch(error => console.log(error));
 }
 
 window.onload = loadGoogleApi;
