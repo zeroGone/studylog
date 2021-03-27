b05f420b0b31fd7e356e2c35bb4088772604e926
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-27 21:16:26 +0900

    feat: 구글로그인 로직 수정
    현재 구글 계정이 연동되어있지 않을 경우의 상태 처리 추가

--- web/js/google_login.js
+++ web/js/google_login.js
@@ -10,33 +10,36 @@ const GOOGLE_LOGIN_INFO = {
 function loadGoogleApi() {
     gapi.load("auth2", () => {
         auth2 = gapi.auth2.init(GOOGLE_LOGIN_INFO.INIT_OPTIONS);
-
         document.querySelector(".google-login-button").addEventListener("click", () => {
-            auth2.signIn().then(login(getGoogleProfile())).catch(handleGoogleApiFail);
+            auth2.signIn().then(login).catch(handleGoogleApiFail);
         });
     });
 }
 
-function getGoogleProfile() {
-    const profile = auth2.currentUser.get().getBasicProfile();
-    return {
-        "name": profile.getName(),
-        "email": profile.getEmail(),
-        "imgUrl": profile.getImageUrl()
+function login() {
+    if (auth2.currentUser) {
+        submit(auth2.currentUser.get().getBasicProfile());
+    } else {
+        auth2.currentUser.listen(submit(auth2.currentUser.get().getBasicProfile()));
     }
+
 }
 
 function handleGoogleApiFail(error) {
     console.error('Google Sign Up or Login Error: ', error)
 }
 
-function login(userInfo) {
+function submit(userProfile) {
     fetch("login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json; charset=utf-8",
         },
-        body: JSON.stringify(userInfo)
+        body: JSON.stringify({
+            "name": userProfile.getName(),
+            "email": userProfile.getEmail(),
+            "imgUrl": userProfile.getImageUrl()
+        })
     }).then(response => {
         if (response.status === 200) {
             window.location.pathname = "mypage";
