280961645d3b067c7e6182f636063816b78b0084
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-27 21:26:14 +0900

    feat: SignupController 이미지 업로드 추가
    이미지업로드 후 유저 생성, 유저 생성이 완료되면 로그인 기능 수행

--- src/main/java/io/zerogone/controller/SignupController.java
+++ src/main/java/io/zerogone/controller/SignupController.java
@@ -1,26 +1,35 @@
 package io.zerogone.controller;
 
+import ch.qos.logback.classic.Logger;
 import io.zerogone.model.ErrorResponse;
-import io.zerogone.user.model.UserDto;
+import io.zerogone.service.FileUploadService;
+import io.zerogone.user.model.CurrentUserInfo;
+import io.zerogone.user.model.UserCreateDto;
 import io.zerogone.user.model.UserVo;
 import io.zerogone.user.service.UserCreateService;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.persistence.PersistenceException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
 public class SignupController {
+    private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
     private final UserCreateService userCreateService;
+    private final FileUploadService fileUploadService;
 
-    public SignupController(UserCreateService userCreateService) {
+    public SignupController(UserCreateService userCreateService, FileUploadService fileUploadService) {
         this.userCreateService = userCreateService;
+        this.fileUploadService = fileUploadService;
     }
 
     @GetMapping("signup")
@@ -34,14 +43,23 @@ public class SignupController {
     }
 
     @PostMapping("signup")
-    public ResponseEntity<Object> handleSignupApi(@RequestBody UserDto userDto, HttpSession httpSession) {
+    public ResponseEntity<Object> handleSignupApi(@ModelAttribute UserCreateDto userCreateDto,
+                                                  HttpSession httpSession,
+                                                  HttpServletRequest httpServletRequest) throws IOException {
+        logger.info("-----create user start-----");
+        logger.debug(String.format("received user data { name : %s, email : %s, nickName : %s }"
+                , userCreateDto.getName(), userCreateDto.getEmail(), userCreateDto.getNickName()));
+
+        if (userCreateDto.getImage() != null) {
+            userCreateDto.setImgUrl(fileUploadService.uploadFile(httpServletRequest.getServletContext().getRealPath("/"), userCreateDto.getImage()));
+        }
         try {
-            UserVo userVo = userCreateService.createUser(userDto);
-            httpSession.setAttribute("userInfo", userVo);
+            UserVo userVo = userCreateService.createUser(userCreateDto);
+            httpSession.setAttribute("userInfo", new CurrentUserInfo(userVo));
             httpSession.removeAttribute("visitor");
             return new ResponseEntity<>(userVo, HttpStatus.CREATED);
         } catch (PersistenceException persistenceException) {
-            return new ResponseEntity<>(new ErrorResponse("NickName or email is Duplicated!"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ErrorResponse("NickName or email is Duplicated!"), HttpStatus.OK);
         }
     }
 }
