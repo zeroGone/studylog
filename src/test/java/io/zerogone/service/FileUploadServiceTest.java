fc0a6865f87750ea7f92e5c986ffba450f3b22ed
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-03-29 20:22:47 +0900

    feat: FileUploadServiceTest 추가

---
+++ src/test/java/io/zerogone/service/FileUploadServiceTest.java
@@ -0,0 +1,43 @@
+package io.zerogone.service;
+
+import io.zerogone.config.DatabaseConfiguration;
+import io.zerogone.config.WebConfiguration;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {WebConfiguration.class, DatabaseConfiguration.class}, loader = AnnotationConfigWebContextLoader.class)
+@WebAppConfiguration
+public class FileUploadServiceTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private FileUploadService fileUploadService;
+
+    @Before
+    public void setUp() {
+        fileUploadService = webApplicationContext.getBean(FileUploadService.class);
+    }
+
+    @Test
+    public void uploadFile() throws IOException {
+        MockMultipartFile firstFile = new MockMultipartFile("data", "filename.txt", "text/plain", "some xml".getBytes());
+        Assert.assertNotNull(fileUploadService.uploadFile("C://tmp", firstFile));
+    }
+
+    @Test
+    public void uploadFile_FileIsNull_ReturnNull() throws IOException {
+        Assert.assertNull(fileUploadService.uploadFile("C://tmp", null));
+    }
+}
