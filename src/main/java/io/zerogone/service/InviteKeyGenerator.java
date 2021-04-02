4bef5d4f78c1ffd627debb5d79675d40fb0020f7
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-04-02 19:06:57 +0900

    feat: 멤버 초대를 위한 키 생성 객체 추가

---
+++ src/main/java/io/zerogone/service/InviteKeyGenerator.java
@@ -0,0 +1,22 @@
+package io.zerogone.service;
+
+public class InviteKeyGenerator {
+    private static final int ASCII_START_CODE = 33;
+    private static final int RANDOM_VALUE = 94;
+
+    private final StringBuilder builder = new StringBuilder();
+
+    public String generateKey(int length) {
+        builder.delete(0, builder.length());
+        for (int number = 1; number <= length; number += 1) {
+            builder.append(getRandomCharacter());
+        }
+        return builder.toString();
+    }
+
+    private char getRandomCharacter() {
+        int randomValue = (int) (Math.random() * RANDOM_VALUE);
+        int randomAsciiCode = randomValue + ASCII_START_CODE;
+        return (char) randomAsciiCode;
+    }
+}
