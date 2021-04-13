a378e173a2ddafc1b01d829d500dd895c0d332e5
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-04-13 18:44:47 +0900

    refactor: 초대키 생성 객체 수정
    키 길이 자신이 갖도록 수정

--- src/main/java/io/zerogone/service/InvitationKeyGenerator.java
+++ src/main/java/io/zerogone/service/InvitationKeyGenerator.java
@@ -3,12 +3,12 @@ package io.zerogone.service;
 public class InvitationKeyGenerator {
     private static final String VALUES =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-
+    private static final int KEY_LENGTH = 15;
     private final StringBuilder builder = new StringBuilder();
 
-    public String generateKey(int length) {
+    public String generateKey() {
         builder.delete(0, builder.length());
-        for (int number = 1; number <= length; number += 1) {
+        for (int number = 1; number <= KEY_LENGTH; number += 1) {
             builder.append(getRandomCharacter());
         }
         return builder.toString();
