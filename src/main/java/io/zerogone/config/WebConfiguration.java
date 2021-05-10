98a59d2ba314dd3ad3b9008c89b319b92743b37c
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-05-10 17:39:53 +0900

    config: ConversionService 빈 등록
    Spring Converter를 사용하기 위한 빈 등록

--- src/main/java/io/zerogone/config/WebConfiguration.java
+++ src/main/java/io/zerogone/config/WebConfiguration.java
@@ -5,12 +5,18 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import io.zerogone.converter.CommentConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
@@ -21,12 +27,20 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 import java.util.Properties;
+import java.util.Set;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "io.zerogone")
 public class WebConfiguration extends WebMvcConfigurerAdapter {
     private static final int TEN_MEGA_BYTE = 10 * 1024 * 1024;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
 
     @Bean
     public ViewResolver viewResolver() {
@@ -35,13 +49,6 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         viewResolver.setPrefix("/WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-        registry.addResourceHandler("/img/**").addResourceLocations("/img/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
     }
 
     @Bean
@@ -54,7 +61,7 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     public AmazonS3 AwsS3Client(@Value("${aws.s3.accesskey}") String accesskey,
-                                @Value("${aws.s3.secretkey}") String secretkey){
+                                @Value("${aws.s3.secretkey}") String secretkey) {
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accesskey, secretkey)))
@@ -79,5 +86,24 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         props.put("mail.debug", "true");
 
         return mailSender;
+    }
+
+    @Bean
+    public ConversionServiceFactoryBean conversionServiceFactory(Set<Converter<?, ?>> converters) {
+        ConversionServiceFactoryBean conversionServiceFactory = new ConversionServiceFactoryBean();
+        conversionServiceFactory.setConverters(converters);
+        return conversionServiceFactory;
+    }
+
+    @Bean
+    public ConversionService conversionService(ConversionServiceFactoryBean factory) {
+        return factory.getObject();
+    }
+
+    @Bean
+    public ConfigurableWebBindingInitializer webBindingInitializer(ConversionService conversionService) {
+        ConfigurableWebBindingInitializer configurableWebBindingInitializer = new ConfigurableWebBindingInitializer();
+        configurableWebBindingInitializer.setConversionService(conversionService);
+        return configurableWebBindingInitializer;
     }
 }
