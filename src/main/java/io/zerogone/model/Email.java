afeb23a303841abd59d8a2bf2f7a6b27cf7186e1
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-05-14 21:25:24 +0900

    feat: 유저 검색 시 Email 유효성 검증 추가

--- src/main/java/io/zerogone/model/Email.java
+++ src/main/java/io/zerogone/model/Email.java
@@ -1,10 +1,17 @@
 package io.zerogone.model;
 
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Validated
 public class Email {
+    @NotBlank(message = "이메일 형식이어야 합니다")
+    @javax.validation.constraints.Email(message = "이메일 형식이어야 합니다")
     private final String value;
 
-    public Email(String value) {
-        this.value = value;
+    public Email(String email) {
+        value = email;
     }
 
     public String getValue() {
