eb354617da9cdce8b209e9e2290511472b3d548e
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-05-15 17:52:22 +0900

    refactor: 마이페이지 검증 추가

--- src/main/java/io/zerogone/controller/MyPageController.java
+++ src/main/java/io/zerogone/controller/MyPageController.java
@@ -3,21 +3,26 @@ package io.zerogone.controller;
 import io.zerogone.model.Email;
 import io.zerogone.model.dto.UserDto;
 import io.zerogone.service.search.SearchService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.validation.Valid;
+
 @Controller
+@Validated
 public class MyPageController {
     private final SearchService<Email, UserDto> searchService;
 
-    public MyPageController(SearchService<Email, UserDto> searchService) {
+    public MyPageController(@Qualifier("userSearchService") SearchService<Email, UserDto> searchService) {
         this.searchService = searchService;
     }
 
     @GetMapping("mypage")
-    public String getMypageViewNameWithBlogVos(@SessionAttribute("userInfo") UserDto userInfo, Model model) {
+    public String getMypageViewNameWithBlogVos(@SessionAttribute("userInfo") @Valid UserDto userInfo, Model model) {
         model.addAttribute("user", searchService.search(new Email(userInfo.getEmail())));
         return "mypage";
     }
