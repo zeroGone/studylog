0cc6399c2936f9b09fdb57dab893337a8b1e37d7
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-05-16 22:49:31 +0900

    refactor: BlogEntityToDtoConverter, BlogSearchService
    블로그만 변환하도록 수정하고 BlogSearchService에서 ConversionService를 이용하여 BlogMemberDto로 변환하도록 로직 수정

--- src/main/java/io/zerogone/service/search/BlogSearchService.java
+++ src/main/java/io/zerogone/service/search/BlogSearchService.java
@@ -3,28 +3,35 @@ package io.zerogone.service.search;
 import io.zerogone.exception.NotExistDataException;
 import io.zerogone.model.BlogName;
 import io.zerogone.model.dto.BlogDto;
+import io.zerogone.model.dto.BlogMemberDto;
 import io.zerogone.model.entity.Blog;
 import io.zerogone.repository.BlogDao;
-import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.NoResultException;
+import java.util.stream.Collectors;
 
 @Service
 public class BlogSearchService implements SearchService<BlogName, BlogDto> {
     private final BlogDao blogDao;
-    private final Converter<Blog, BlogDto> converter;
+    private final ConversionService conversionService;
 
-    public BlogSearchService(BlogDao blogDao, Converter<Blog, BlogDto> converter) {
+    public BlogSearchService(BlogDao blogDao, ConversionService conversionService) {
         this.blogDao = blogDao;
-        this.converter = converter;
+        this.conversionService = conversionService;
     }
 
     @Override
     public BlogDto search(BlogName blogName) {
         try {
             Blog entity = blogDao.findByName(blogName.get());
-            return converter.convert(entity);
+            BlogDto blogDto = conversionService.convert(entity, BlogDto.class);
+            blogDto.setMembers(entity.getMembers()
+                    .stream()
+                    .map(member -> conversionService.convert(member, BlogMemberDto.class))
+                    .collect(Collectors.toList()));
+            return blogDto;
         } catch (NoResultException noResultException) {
             throw new NotExistDataException("검색한 블로그가 없습니다", blogName.get());
         }
