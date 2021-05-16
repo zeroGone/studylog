0851d1841381d6d2e4ab825d279dfc3ba1108f5f
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-05-16 22:42:19 +0900

    refactor: UserEntityToDtoConverter, UserSearchService
    유저만 바꾸도록 blog 바꾸는 부분 삭제.
    이에 따라 UserSearchService 로직 수정

--- src/main/java/io/zerogone/converter/UserEntityToDtoConverter.java
+++ src/main/java/io/zerogone/converter/UserEntityToDtoConverter.java
@@ -1,14 +1,9 @@
 package io.zerogone.converter;
 
-import io.zerogone.model.dto.BlogDto;
 import io.zerogone.model.dto.UserDto;
-import io.zerogone.model.entity.Blog;
 import io.zerogone.model.entity.User;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class UserEntityToDtoConverter implements Converter<User, UserDto> {
@@ -20,21 +15,6 @@ public class UserEntityToDtoConverter implements Converter<User, UserDto> {
         dto.setName(entity.getName());
         dto.setNickName(entity.getNickName());
         dto.setImageUrl(entity.getImageUrl());
-        dto.setBlogs(convertBlogs(entity.getBlogs()));
         return dto;
-    }
-
-    private List<BlogDto> convertBlogs(List<Blog> blogs) {
-        return blogs.stream()
-                .map(blog -> {
-                    BlogDto dto = new BlogDto();
-                    dto.setId(blog.getId());
-                    dto.setName(blog.getName());
-                    dto.setIntroduce(blog.getIntroduce());
-                    dto.setImageUrl(blog.getImageUrl());
-                    return dto;
-                })
-                .collect(Collectors.toList());
-
     }
 }
--- src/main/java/io/zerogone/service/search/UserSearchService.java
+++ src/main/java/io/zerogone/service/search/UserSearchService.java
@@ -2,29 +2,36 @@ package io.zerogone.service.search;
 
 import io.zerogone.exception.NotExistDataException;
 import io.zerogone.model.Email;
+import io.zerogone.model.dto.BlogDto;
 import io.zerogone.model.dto.UserDto;
 import io.zerogone.model.entity.User;
 import io.zerogone.repository.UserDao;
-import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.NoResultException;
+import java.util.stream.Collectors;
 
 @Service
 public class UserSearchService implements SearchService<Email, UserDto> {
     private final UserDao userDao;
-    private final Converter<User, UserDto> converter;
+    private final ConversionService conversionService;
 
-    public UserSearchService(UserDao userDao, Converter<User, UserDto> converter) {
+    public UserSearchService(UserDao userDao, ConversionService conversionService) {
         this.userDao = userDao;
-        this.converter = converter;
+        this.conversionService = conversionService;
     }
 
     @Override
     public UserDto search(Email email) {
         try {
             User entity = userDao.findByEmail(email.get());
-            return converter.convert(entity);
+            UserDto dto = conversionService.convert(entity, UserDto.class);
+            dto.setBlogs(entity.getBlogs()
+                    .stream()
+                    .map(blog -> conversionService.convert(blog, BlogDto.class))
+                    .collect(Collectors.toList()));
+            return dto;
         } catch (NoResultException noResultException) {
             throw new NotExistDataException("검색한 유저가 없습니다", email.get());
         }
