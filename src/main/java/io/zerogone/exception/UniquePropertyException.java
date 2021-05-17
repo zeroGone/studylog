a7eec2dc6e0606a4ac80fbbd0b2acfd7639c99f4
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-05-17 19:08:42 +0900

    refactor: validation exception 처리로 커스텀 예외들 삭제

--- src/main/java/io/zerogone/exception/UniquePropertyException.java
+++
@@ -1,7 +0,0 @@
-package io.zerogone.exception;
-
-public class UniquePropertyException extends CustomRuntimeException {
-    public UniquePropertyException(String message, Object exceptionValue) {
-        super(message, exceptionValue);
-    }
-}
