6651c2a99e9830cf5c86294d10786405817cf6a8
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-05-21 15:26:46 +0900

    refactor: 로그아웃
    LoginCheckFilter에서 logout 필터링, RestController에서 Controller로 변경

--- src/main/java/io/zerogone/user/controller/LogoutController.java
+++ src/main/java/io/zerogone/user/controller/LogoutController.java
@@ -1,16 +1,15 @@
 package io.zerogone.user.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 public class LogoutController {
-    @PostMapping("api/logout")
-    public ResponseEntity<Object> doLogout(HttpSession httpSession) {
+    @PostMapping("logout")
+    public String doLogout(HttpSession httpSession) {
         httpSession.removeAttribute("userInfo");
-        return ResponseEntity.ok("logout success");
+        return "redirect:/";
     }
 }
